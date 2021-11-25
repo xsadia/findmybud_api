de20171c53068b79fc39c95974fd2502e4b4d49f
xsadia <lipearthur@hotmail.com>
2021-11-24 22:54:35 -0300

    feat: add Post entity model :monkey:

---
+++ src/models/Post.ts
@@ -0,0 +1,47 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface IPost extends Document {
+  postType: "found" | "lost";
+  author: Types.ObjectId;
+  location: string;
+  content: string;
+  comments: Types.Array<Types.ObjectId>;
+  likes: Types.Array<Types.ObjectId>;
+}
+
+const PostSchema = new Schema(
+  {
+    postType: {
+      type: String,
+      enum: ["found", "lost"],
+      default: "lost",
+    },
+    author: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    comment: [
+      {
+        type: Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    likes: [
+      {
+        type: Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Post = model<IPost>("Post", PostSchema);
