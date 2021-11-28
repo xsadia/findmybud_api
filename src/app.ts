000cc8767fac8892b52ebe028262b7eac20dfd02
xsadia <lipearthur@hotmail.com>
2021-11-28 00:35:00 -0300

    feat: add post routes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import convert from "koa-convert";
 import cors from "koa-cors";
 import bodyParser from "koa-bodyparser";
 import { userRouter } from "./routes/userRoutes";
+import { postRouter } from "./routes/postRoutes";
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(convert(cors()));
 app.use(userRouter.routes()).use(userRouter.allowedMethods());
+app.use(postRouter.routes()).use(postRouter.allowedMethods());
 
 export default app;
