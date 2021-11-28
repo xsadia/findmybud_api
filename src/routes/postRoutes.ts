1399c7a3ff63d1276a156793211f347c1f175746
xsadia <lipearthur@hotmail.com>
2021-11-28 17:24:33 -0300

    add getPosts and getPost routes

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,14 +1,15 @@
-import { Context } from "koa";
 import Router from "koa-router";
+import { createPostController } from "../controllers/PostControllers/CreatePostController";
+import { getPostController } from "../controllers/PostControllers/GetPostController";
+import { getPostsController } from "../controllers/PostControllers/GetPostsController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 export const postRouter = new Router({
   prefix: "/posts",
 });
 
-postRouter.get("/", ensureAuthenticated, (ctx: Context) => {
-  const { _id } = ctx.user;
-  ctx.body = {
-    user: _id,
-  };
-});
+postRouter.post("/", ensureAuthenticated, createPostController);
+
+postRouter.get("/", getPostsController);
+
+postRouter.get("/:id", getPostController);
