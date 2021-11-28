bd0250c3be25743850828b41b18ad8131c2a8837
xsadia <lipearthur@hotmail.com>
2021-11-28 17:39:11 -0300

    refactor: fix _id type to be Types.ObejectId

--- src/api/post/GetPost.ts
+++ src/api/post/GetPost.ts
@@ -1,7 +1,8 @@
+import { Types } from "mongoose";
 import { AppError } from "../../error/AppError";
 import { IPost, Post } from "../../models/Post";
 
-export const GetPost = async (_id: string): Promise<IPost> => {
+export const GetPost = async (_id: Types.ObjectId): Promise<IPost> => {
     const postExists = await Post.findOne({ _id });
     if (!postExists) {
         throw new AppError('Post not found', 404);
