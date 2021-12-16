394c59a04d058e4202cfcfb7edd178097f35696b
xsadia <lipearthur@hotmail.com>
2021-12-16 02:14:05 -0300

    feat: add postType to node definitions

--- src/graphql/node/nodeDefinition.ts
+++ src/graphql/node/nodeDefinition.ts
@@ -1,20 +1,30 @@
 import { fromGlobalId, nodeDefinitions } from "graphql-relay";
+import { Post } from "../../models/Post";
 import { User } from "../../models/User";
+import { PostType } from "../post/PostType";
 import { UserType } from "../user/UserType";
 
 export const { nodeInterface, nodeField, nodesField } = nodeDefinitions(
-    async (globalId) => {
-        const { id, type } = fromGlobalId(globalId);
+  async (globalId) => {
+    const { id, type } = fromGlobalId(globalId);
 
-        if (type === "User") {
-            return await User.findOne({ _id: id });
-        }
-
-        return null;
-    },
-    (obj) => {
-        if (obj instanceof User) {
-            return UserType;
-        }
+    if (type === "User") {
+      return await User.findOne({ _id: id });
     }
+
+    if (type === "Post") {
+      return await Post.findOne({ _id: id });
+    }
+
+    return null;
+  },
+  (obj) => {
+    if (obj instanceof User) {
+      return UserType;
+    }
+
+    if (obj instanceof Post) {
+      return PostType;
+    }
+  }
 );
